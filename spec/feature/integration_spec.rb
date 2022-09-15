0fe7de769d5ab6d4f6af30ee31faaec45fbf6629
capito2000 <aaronaranda00@gmail.com>
2022-09-14 20:21:51 -0500

    Fiinished

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Aaron Aranda'
+    #fill_in 'Price', with: '12.0'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
